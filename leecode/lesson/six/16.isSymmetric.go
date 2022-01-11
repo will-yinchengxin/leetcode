3444acff6ffc6a278a60f4aa5b547bf0854699bd
chengxin.yin <826895143@qq.com>
2022-01-11 18:35:09 +0800

    feat: add some code

---
+++ leecode/lesson/six/16.isSymmetric.go
@@ -0,0 +1,21 @@
+package six
+/*
+对称二叉树
+
+给你一个二叉树的根节点 root ， 检查它是否轴对称。
+
+示例 1：
+	输入：root = [1,2,2,3,4,4,3]
+	输出：true
+
+示例 2：
+	输入：root = [1,2,2,null,3,null,3]
+	输出：false
+
+提示：
+	树中节点数目在范围 [1, 1000] 内
+	-100 <= Node.val <= 100
+*/
+func isSymmetric(root *TreeNode) bool {
+
+}

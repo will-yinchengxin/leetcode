7708f752377d01647662a2577b32a89f5d7a8c66
chengxin.yin <826895143@qq.com>
2022-01-12 18:12:08 +0800

    feat: finish the week-six

--- leecode/lesson/six/14.mergeTrees.go
+++ leecode/lesson/six/14.mergeTrees.go
@@ -26,5 +26,22 @@ package six
 注意:合并必须从两个树的根节点开始。
 */
 func mergeTrees(root1 *TreeNode, root2 *TreeNode) *TreeNode {
+	/*
+		1) 可以拆分为子问题进行求解
+			两颗树的左子树的和作为新的左子树的val
+			两颗树的右子树的和作为新的右子树的val
+			其中一个为 nil 直接返回即可
+	*/
+	if root1 == nil {
+		return root2
+	}
+	if root2 == nil {
+		return root1
+	}
+	newNode := new(TreeNode)
+	newNode.Val = root1.Val + root2.Val
+	newNode.Left = mergeTrees(root1.Left, root2.Left)
+	newNode.Right = mergeTrees(root1.Right, root2.Right)
 
+	return newNode
 }
--- leecode/lesson/six/16.isSymmetric.go
+++ leecode/lesson/six/16.isSymmetric.go
@@ -5,10 +5,20 @@ package six
 给你一个二叉树的根节点 root ， 检查它是否轴对称。
 
 示例 1：
+		 4
+	   /   \
+	  2     2
+	 / \   / \
+	3   4 4   3
 	输入：root = [1,2,2,3,4,4,3]
 	输出：true
 
 示例 2：
+		 1
+	   /   \
+	  2     2
+	   \     \
+	    3     3
 	输入：root = [1,2,2,null,3,null,3]
 	输出：false
 
@@ -17,5 +27,32 @@ package six
 	-100 <= Node.val <= 100
 */
 func isSymmetric(root *TreeNode) bool {
+	/*
+		1) 不能分解为最小问题
+		2) 以镜像树来思路考虑问题,
+				 4
+			   /   \
+			  2     2
+			 / \   / \
+			1   3 3   1
+		- 最左边 VS 最右边
+		- 左二  VS 右二
 
+	*/
+	if root == nil {
+		return true
+	}
+	return isSymmetric_R(root.Left, root.Right)
+}
+func isSymmetric_R(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+	if (left != nil && right == nil) || (right != nil && left == nil) {
+		return false
+	}
+	if (left != nil && right != nil) && (left.Val == right.Val) {
+		return isSymmetric_R(left.Left, right.Right) && isSymmetric_R(left.Right, right.Left)
+	}
+	return false // (left != nil && right != nil) && (left.Val != right.Val)
 }

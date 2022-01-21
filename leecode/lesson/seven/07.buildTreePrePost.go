514fd2e7ca2e5f147f90bede59b16f3a9abaea0c
chengxin.yin <826895143@qq.com>
2022-01-21 15:50:33 +0800

    feat: finish the week-seven

--- leecode/lesson/seven/07.buildTreePrePost.go
+++ leecode/lesson/seven/07.buildTreePrePost.go
@@ -28,17 +28,37 @@ func constructFromPrePost(preorder []int, postorder []int) *TreeNode {
 		但是拆解到最后发现，除了能够快速确定根节点以外， 不能够准确的区分左右子树
 
 		那么另辟蹊径， 发现前序遍历的 root 节点后的一个节点很神奇，它可能是左子树的根节点， 也可能是右子树的根节点
+		- 存在 左子树 或者 左子树和右子树 都存在 为 左子树
+		- 只存在 右子树 的时候 为右子树
+
+		存在一种特殊情况:
+			  1
+			 /
+			2
+		   / \
+		   3  4
+		   前： 1 2 3 4
+		   后： 3 4 2 1
+
+			1
+			 \
+			 2
+			/ \
+			3  4
+			前： 1 2 3 4
+			后： 3 4 2 1
+		解决办法：这种情况会有两种解答，那么可以统一使用左子树输出即可
 	*/
-	return myBuild_PrePost(preorder, 0, len(preorder), postorder, 0, len(postorder))
+	return myBuildTree_Ano(preorder, 0, len(preorder)-1, postorder, 0, len(postorder)-1)
 }
-func myBuild_PrePost(preorder []int, i int, j int, postorder[]int, p int, r int) *TreeNode {
+func myBuildTree_Ano(preorder []int, i int, j int, postorder[]int, p int, r int) *TreeNode {
 	if i > j {
 		return nil
 	}
 	root := &TreeNode{
 		Val: preorder[i],
 	}
-	if i == j {  // 与前中遍历内容不一致的位置
+	if i == j {  // 与前中遍历内容不一致的位置, 避免后续逻辑造成 数组越界 的错误
 		return root
 	}
 
@@ -46,10 +66,10 @@ func myBuild_PrePost(preorder []int, i int, j int, postorder[]int, p int, r int)
 	for postorder[q] != preorder[i+1] {
 		q++
 	}
-	leftTreeSize := q - p // 左子树得长度范围（左子树内容）
+	leftTreeSize := q - p + 1// 左子树得长度范围（左子树内容）
 
-	leftNode := myBuildTree(preorder, i+1, i+leftTreeSize, postorder, p, q)
-	rightNode := myBuildTree(preorder, i+1+leftTreeSize, j, postorder, q+1, r-1)
+	leftNode := myBuildTree_Ano(preorder, i+1, i+leftTreeSize, postorder, p, q)
+	rightNode := myBuildTree_Ano(preorder, i+1+leftTreeSize, j, postorder, q+1, r-1)
 	root.Left = leftNode
 	root.Right = rightNode
 	return root
--- leecode/lesson/seven/summary.go
+++ leecode/lesson/seven/summary.go
@@ -1,4 +1,5 @@
 package seven
+
 /*
 一. 二叉树
 	题型套路：
@@ -14,8 +15,77 @@ package seven
 
 
 三. 堆
+	- 堆必须是一个完全二叉树
+	- 堆里每个节点的值必须 大于等于（小于等于）其子树中每个节点的值， 也就是堆中每个节点的值都 大于等于（小于等于）其左右子节点的值
 
+	如果堆中每个节点的值都 大于等于 其子树中每个节点的值， 我们成为 大顶堆
+	如果堆中每个节点的值都 小于等于 其子树中每个节点的值， 我们成为 小顶堆
 
+	堆是完全二叉树， 所以其适合使用数组来存储(非完全二叉树使用数组存储会产生很多空洞， 适合使用链表来存储
+
+					12
+				   /   \
+				  9     6
+				/  \   /  \
+			   7    8  3   5
+			  /
+			 4
+
+	root = 1， 左：2i， 右：2i + 1
+
+	堆上的操作
+		- 堆中插入数据
+			将新的数据插入到末尾， 然后执行自下而上的堆化 // 时间复杂度 O(h)
+			type Heap struct {
+				arr   []int // 数组， 从下标 1 开始存储
+				n     int	// 可以存储的最大数据个数
+				count int	// 已经存储的数据个数
+			}
+
+			func initHeap(cap int) *Heap {
+				return &Heap{
+					arr: make([]int, 0 , cap+1),
+					n: cap,
+					count: 0,
+				}
+			}
+			func (h *Heap) inset(data int) {
+				if h.count >= h.n {
+					return // 满了
+				}
+				h.count++
+				h.arr[h.count] = data
+				i := h.count
+				for i/2 > 0 && h.arr[i] > h.arr[i/2] { // 自下网上堆化
+					swap(h.arr, i, i/2)
+				}
+			}
+			func swap(arr []int, i, j int) {
+				tmp := arr[i]
+				arr[i] = arr[j]
+				arr[j] = tmp
+			}
+
+		- 取堆顶元素
+			func top() {
+				if count == 0 {
+					return min_value/max_value
+				}
+				return a[1]
+			}
+			大顶堆取最大元素， 小顶堆取最小元素
+
+		- 删除堆顶元素
+			把最后一个节点放置堆顶，进行自上而下的堆化方式 // O(h)
+
+		- 更新元素值
+			如果更新之后的值变小了，就进行自上而下的堆化
+			如果更新之后的值变大了，就进行自下而上的堆化
+
+	堆排序：
+		包含两个大步骤：
+			- 建堆， 将数组中的数据原地组织成一个堆
+			- 排序， 基于堆的排序数据
 */
 type TreeNode struct {
 	Val   int
